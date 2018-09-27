c3305ac0f03126020a0cd55ab99bdaab203dafd9
jonathanTIE <jonathan_bleu@hotmail.fr>
2018-09-27 19:32:07 +0200

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -53,8 +53,6 @@ async def on_ready():
 				LIST_NOTIF_CHANNEL[LIST_NOTIF_CHANNEL.index(y)] = x.id
 		if str(x) == str(CHANNEL_FOR_NOTIF):
 			CHANNEL_FOR_NOTIF = x
-	print(CHANNEL_FOR_NOTIF)
-	print(LIST_NOTIF_CHANNEL)
 	CLIENT.loop.create_task(infinite_check())
 				
 @CLIENT.event
@@ -64,7 +62,6 @@ async def infinite_check():
 		for idChannel in LIST_NOTIF_CHANNEL:
 			voiceMembers = CLIENT.get_channel(idChannel).voice_members
 			if  voiceMembers and CurVoiceMembers != voiceMembers:
-				print("Found someone in channel")
 				CurChannel = CLIENT.get_channel(idChannel)
 				await CLIENT.send_message(CHANNEL_FOR_NOTIF,
 				content=":alerte: Quelqu'un s'est connecté sur le channel {0}! {1} est/sont présent(s) ! @here :alerte:".format(
@@ -73,7 +70,7 @@ async def infinite_check():
 			elif not voiceMembers:
 				CurVoiceMembers = []
 			else:
-				print(CLIENT.get_channel(idChannel).voice_members)
+				pass
 		await asyncio.sleep(1)
 		
 CLIENT.run(TOKEN)
