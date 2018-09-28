6090c1673c7b001d8680582720d1dd63a3be841b
jonathanTIE <jonathan_bleu@hotmail.fr>
2018-09-28 20:30:53 +0200

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -58,8 +58,8 @@ async def on_ready():
 @CLIENT.event
 async def infinite_check():
 	CurVoiceMembers = []
-	print(CurVoiceMembers)
 	while True:
+		print(CurVoiceMembers)
 		for idChannel in LIST_NOTIF_CHANNEL:
 			voiceMembers = CLIENT.get_channel(idChannel).voice_members
 			if  voiceMembers and CurVoiceMembers != voiceMembers:
